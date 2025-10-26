3cba569cae7443e3702495f96701f1aae31f85c0
Vitor Guidi <vitorguidi@gmail.com>
2025-10-25 22:34:47 -0300

    Pattern matching again

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-fn main() {
-    let t: (i32, i32, bool) = (2, 3, false);
-    let (mut x,..,mut y) = t;
-    println!("{}", x);
-    println!("{}", y);
-    (_, x, y) = t;
-    println!("{}", x);
-    println!("{}", y);
-    let (.., w) = t;
-    println!("{}", w); 
+use console::Term;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let term = Term::stdout();
+    println!("Press any key to continue...");
+    let c = term.read_char()?;
+    match c {
+        '0'..='9' => println!("Your digit is a number: {}", c),
+        'w' | 'a' | 's' | 'd' => println!("Your digit is a move: {}", c),
+        key if key != '#' => println!("Not a #: {}", c),
+        _ => println!("Hashtag!!! {}", c)
+    }
+    Ok(())
 }
