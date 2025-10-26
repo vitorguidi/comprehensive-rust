37cae3b03b5529e88c656b97286b767feb8e0903
Vitor Guidi <vitorguidi@gmail.com>
2025-10-25 21:48:49 -0300

    String and &str

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
 fn main() {
-    let a: [i32; 3] = [1,2,3];
-    let r = &a[0..2];
-    dbg!(r);
-    for item in r {
-        println!("{}", item);
-    }
-    let mut b: [i32; 3] = [4,5,6];
-    let s = &mut b[0..2];
-    s[0]=10;
-    s[1]=20;
+    let s: String = "abcde".to_string();
+    let t: &str = &s;
+
     dbg!(s);
-    dbg!(b);
+    dbg!(t);
 }
