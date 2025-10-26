87ca6158826d5d5894000e41d7cf82d50e001287
Vitor Guidi <vitorguidi@gmail.com>
2025-10-25 22:41:07 -0300

    Struct deref

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
-use console::Term;
+struct Point {
+    x: f64,
+    y: f64,
+}
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let term = Term::stdout();
-    println!("Press any key to continue...");
-    let c = term.read_char()?;
-    match c {
-        '0'..='9' => println!("Your digit is a number: {}", c),
-        'w' | 'a' | 's' | 'd' => println!("Your digit is a move: {}", c),
-        key if key != '#' => println!("Not a #: {}", c),
-        _ => println!("Hashtag!!! {}", c)
+fn evaluate(p: &Point) {
+    match p {
+        Point{x:_, y:5.0} => println!("Point ends with 5"),
+        Point{x:5.0, y:_} => println!("Point starts with 5"),
+        Point{x,y} => {println!("Something else: {} {}", x, y)}
     }
-    Ok(())
+}
+
+fn main() {
+    let p1: Point = Point{x:1.0,y:5.0};
+    let p2: Point = Point{x:5.0,y:3.0};
+    let p3: Point = Point{x:3.0,y:3.0};
+    evaluate(&p1);
+    evaluate(&p2);
+    evaluate(&p3);
 }
