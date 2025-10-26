08f6dbb6b61ea8064cc3a7c8008ce4cc18a01761
Vitor Guidi <vitorguidi@gmail.com>
2025-10-25 21:27:13 -0300

    destructure

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod collatz;
 
 fn main() {
-    let t: (i32, bool, [i32; 8]) = (2, true, [1,2,3,4,5,6,7,8]);
-    println!("{}", t.0);
-    println!("{}", t.1);
-    for item in t.2 {
-        println!("{}", item);
-    }
+    let t: (i32, bool) = (2, true);
+    let (val, boolval) = t;
+    println!("{}", val);
+    println!("{}", boolval);
+    let [x,y,z] = [1,2,3];
+    println!("{}", x);
+    println!("{}", y);
+    println!("{}", z);
 }
