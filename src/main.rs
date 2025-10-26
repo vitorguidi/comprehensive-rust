ece2875e89c466bcf58065a083e8d087c1b30e47
Vitor Guidi <vitorguidi@gmail.com>
2025-10-25 21:41:06 -0300

    refs and slices

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-mod collatz;
-
 fn main() {
-    let t: (i32, bool) = (2, true);
-    let (val, boolval) = t;
-    println!("{}", val);
-    println!("{}", boolval);
-    let [x,y,z] = [1,2,3];
-    println!("{}", x);
-    println!("{}", y);
-    println!("{}", z);
+    let a: [i32; 3] = [1,2,3];
+    let r = &a[0..2];
+    dbg!(r);
+    for item in r {
+        println!("{}", item);
+    }
+    let mut b: [i32; 3] = [4,5,6];
+    let s = &mut b[0..2];
+    s[0]=10;
+    s[1]=20;
+    dbg!(s);
+    dbg!(b);
 }
