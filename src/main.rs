5248fece7e1c1c8549beffa348fd9a92e94e93c1
Vitor Guidi <vitorguidi@gmail.com>
2025-10-25 22:50:09 -0300

    Destruct ok and error

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,16 @@
-struct Point {
-    x: f64,
-    y: f64,
-}
-
-fn evaluate(p: &Point) {
-    match p {
-        Point{x:_, y:5.0} => println!("Point ends with 5"),
-        Point{x:5.0, y:_} => println!("Point starts with 5"),
-        Point{x,y} => {println!("Something else: {} {}", x, y)}
-    }
+fn divide(x: i32, y: i32) -> Result<i32, &'static str> {
+    let result = match (x,y) {
+        (x,y) if y!=0 => Ok(x/y),
+        _ => Err("Cannot divide by zero.")
+    };
+    match result {
+        Ok(value) => println!("Division successful: {}/{} = {}", x, y, value),
+        Err(msg) => println!("Error: {}", msg)
+    };
+    result
 }
 
 fn main() {
-    let p1: Point = Point{x:1.0,y:5.0};
-    let p2: Point = Point{x:5.0,y:3.0};
-    let p3: Point = Point{x:3.0,y:3.0};
-    evaluate(&p1);
-    evaluate(&p2);
-    evaluate(&p3);
+    assert_eq!(Err("Cannot divide by zero."), divide(2,0));
+    assert_eq!(divide(2,2),Ok(1));
 }
