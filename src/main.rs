5a415739632eda0c374e49221bb926bd162a6915
Vitor Guidi <vitorguidi@gmail.com>
2025-10-25 21:57:05 -0300

    Normalization of 3d point

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
-fn main() {
-    let s: String = "abcde".to_string();
-    let t: &str = &s;
+// Calculate the magnitude of a vector by summing the squares of its coordinates
+// and taking the square root. Use the `sqrt()` method to calculate the square
+// root, like `v.sqrt()`.
 
-    dbg!(s);
-    dbg!(t);
+fn magnitude(v: &[f64; 3]) -> f64 {
+    let sumofsquares: f64 = v[0]*v[0] + v[1]*v[1] + v[2]*v[2];
+    sumofsquares.sqrt()
+}
+
+// Normalize a vector by calculating its magnitude and dividing all of its
+// coordinates by that magnitude.
+
+
+fn normalize(v: &mut [f64; 3]) {
+    let norm: f64 = magnitude(v);
+    v[0]/= norm;
+    v[1]/= norm;
+    v[2]/= norm;
+}
+
+// Use the following `main` to test your work.
+
+fn main() {
+    println!("Magnitude of a unit vector: {}", magnitude(&[0.0, 1.0, 0.0]));
+
+    let mut v = [1.0, 2.0, 9.0];
+    println!("Magnitude of {v:?}: {}", magnitude(&v));
+    normalize(&mut v);
+    println!("Magnitude of {v:?} after normalization: {}", magnitude(&v));
 }
